c8249a4bb60c2f9c0e1f423d0c445244f7830b62
Ewout Klimbie <90047337+eklimbie@users.noreply.github.com>
2025-08-10 17:18:45 +0200

    Updated NixNuc NixOS configuration to generation 202

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -91,6 +91,7 @@ in
           insteadOf = "https://github.com/";
         };
       };
+      advice.addIgnoredFile = false; # Removes message to reming me that some of the added files are being ignored
     };
   };
 
