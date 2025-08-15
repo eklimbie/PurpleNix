c0a8ed857c8f12131964151dee8625cef6bb3ee1
Ewout Klimbie <90047337+eklimbie@users.noreply.github.com>
2025-08-15 18:36:37 +0200

    Updated your NixOS config bringing Babbage to generation 21

--- modules/l18n.nix
+++ modules/l18n.nix
@@ -30,8 +30,8 @@
       LC_NUMERIC = "nl_NL.UTF-8";
       LC_PAPER = "nl_NL.UTF-8";
       LC_TELEPHONE = "de_DE.UTF-8";
-      # LC_TIME = "en_GB.UTF-8";
-      LC_TIME = "nl_NL.UTF-8";
+      LC_TIME = "en_GB.UTF-8";
+      # LC_TIME = "nl_NL.UTF-8";
     };
   };
 
